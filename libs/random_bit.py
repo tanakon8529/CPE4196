df7c0b1a21f67e17abc40e94d62ded312e001476
tanakon8529 <tanakon.kabprapun@gmail.com>
2022-01-17 19:43:02 +0700

    save

--- libs/random_bit.py
+++ libs/random_bit.py
@@ -3,20 +3,20 @@ from random import randrange
 def random_bit():
     space_size = []
     for count in range(10):
-    space_in = []
-    for count in range(5):
-        space_in.append(randrange(2))
+        space_in = []
+        for count in range(5):
+            space_in.append(randrange(2))
 
-    space_size.append(space_in)
+        space_size.append(space_in)
 
 
     bit_25 = []
     first_impro = []
     for count in range(25):
-    x = randrange(10)
-    y = randrange(5)
-    if first_impro == []:
-        first_impro = "[{}][{}]".format(x+1, y+1)
-    bit_25.append(space_size[x][y])
+        x = randrange(10)
+        y = randrange(5)
+        if first_impro == []:
+            first_impro = "[{}][{}]".format(x+1, y+1)
+        bit_25.append(space_size[x][y])
 
     return space_size, bit_25, first_impro
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from libs.random_bit import random_bit
 
 space_size, bit25, first_impro = random_bit()
+count_row = 1
 
 print("===space_size===")
 for row in space_size:
-    print(row)
+    print("{} - {}".format(count_row, row))
+    count_row +=1
+
 print("===============")
 print("First-improvement : {}".format(first_impro))
 print("25 bit problem : ", bit25)
