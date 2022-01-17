917d8eeed46a28b485ec490e4a7d5949efbe8590
tanakon8529 <tanakon.kabprapun@gmail.com>
2022-01-17 11:40:21 +0700

    v0.1

--- 6451000977/Assigment_1/1.py
+++ 6451000977/Assigment_1/1.py
@@ -1,1 +1,17 @@
-import  ra
+from random import random, randrange
+
+space_size = []
+for count in range(10):
+    space_in = []
+    for count in range(5):
+        space_in.append(randrange(2))
+    
+    space_size.append(space_in)
+
+
+
+bit_25 = []
+for count in range(25):
+    bit_25.append(space_size[randrange(10)][randrange(5)])
+
+print(bit_25)
