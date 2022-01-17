88942ee3ceb4705abf482809cdf9c408a84c76ee
tanakon8529 <tanakon.kabprapun@gmail.com>
2022-01-17 19:35:13 +0700

    function save

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from libs.random_bit import random_bit
 
-bit25 = random_bit()
-print(bit25)
+space_size, bit25, first_impro = random_bit()
+
+print("===space_size===")
+for row in space_size:
+    print(row)
+print("===============")
+print("First-improvement : {}".format(first_impro))
+print("25 bit problem : ", bit25)
